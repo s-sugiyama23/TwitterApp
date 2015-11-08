e9bb77d3dc214845376817b0dd3a4ed17b29beaf
s-sugiyama23 <s.sugiyama2310@gmail.com>
2015-11-08 12:18:00 +0900

    test commit

---
+++ src/main/java/com/example/TwitterAppApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterAppApplication.class, args);
+    }
+}
